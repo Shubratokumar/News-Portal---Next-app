17b868b67ba0f21392b1a6b482e56d6c3bae7e67
Shubrato Kumar <shubratokumargharami920@gmail.com>
2024-08-30 23:45:11 +0600

    main header added

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 const NewsDetails = ({params}) => {
     const slug = params.slug;
   return (
     <div>
         <h1>News Details Here</h1>
-        <h4>Slug: {slug}</h4>
+        <h4>Slug: {slug} </h4>
         
     </div>
   )
