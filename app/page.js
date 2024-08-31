b1210938e35fc96733a0b97053b4cfba9c090e5c
Shubrato Kumar <shubratokumargharami920@gmail.com>
2024-08-31 12:03:27 +0600

    button imporved

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-between p-24">
-      <Link className="px-3 py-2 rounded-lg transition-colors ease-in-out text-white hover:bg-slate-200 hover:text-black" href="/news">Explore News</Link>
+      <Button variant="ghost" className="transition-colors ease-in-out text-white hover:text-black">
+        <Link  href="/news">
+          Explore News
+        </Link>
+      </Button>
     </main>
   );
 }
--- components/main-header.jsx
+++ components/main-header.jsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const MainHeader = () => {
   return (
     <header className="flex items-center justify-between">
-        <div>
-            <Link className="text-white text-2xl font-semibold" href="/">Next News</Link> 
-        </div>
+      <div>
+        <Link className="text-white text-2xl font-semibold" href="/">
+          Next News
+        </Link>
+      </div>
       <nav>
         <ul>
-          <li className="px-3 py-2 rounded-lg transition-colors ease-in-out text-white hover:bg-slate-200 hover:text-black">
-            <Link className="text-lg font-sans font-semibold" href="/news">News</Link>
+          <li>
+            <Button
+              variant="ghost"
+              className="transition-colors ease-in-out text-white hover:text-black"
+            >
+              <Link href="/news" className="text-lg font-sans font-semibold">
+                News
+              </Link>
+            </Button>
           </li>
         </ul>
       </nav>
